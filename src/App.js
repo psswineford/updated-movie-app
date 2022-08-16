9683ee462908f55a953a544d5a74a8804f6436bf
Shawn Swineford <psswineford@gmail.com>
2022-08-16 10:45:35 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
+import AddFavorite from "./components/AddFavorites";
+import RemoveFavorites from "./components/RemoveFavorites";
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  const getMovieRequest = async (searchValue) => {
+     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=edbfcd53`
+    
+     const response = await fetch(url);
+     const responseJson = await response.json();
+
+     if(responseJson.Search){
+     setMovies(responseJson.Search)
+     }
+  };
+
+  useEffect(()=>{
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
+  useEffect(() => {
+		const movieFavorites = JSON.parse(
+			localStorage.getItem('react-movie-app-favorites')
+		);
+
+		setFavorites(movieFavorites);
+	}, []);
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('react-movie-app-favorites', JSON.stringify(items));
+  };
+
+  const AddFavoriteMovie = (movie) => {
+    const newFavoriteList = [...favorites, movie];
+    setFavorites(newFavoriteList);
+    saveToLocalStorage(newFavoriteList);
+  }
+
+  const removeFavoriteMovie = (movie) => {
+    const newFavoriteList = favorites.filter(
+      (favorite) => favorite.imdbID !== movie.imdbID
+    );
+
+    setFavorites(newFavoriteList);
+    saveToLocalStorage(newFavoriteList);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid movie-app'>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading='MyFlix'/>
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+      </div>
+      <div className="row">
+        <MovieList movies={movies} handeFavoritesClick={AddFavoriteMovie} favoriteComponent = {AddFavorite} />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading='Favorites'/>
+      </div>
+      <div className="row">
+        <MovieList movies={favorites} handeFavoritesClick={removeFavoriteMovie} favoriteComponent = {RemoveFavorites} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;

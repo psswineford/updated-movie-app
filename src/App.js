1891439e3fec823b34010640da958768a2b062ba
Shawn Swineford <psswineford@gmail.com>
2022-09-21 12:44:45 -0700

    updated title

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ const App = () => {
   return (
     <div className='container-fluid movie-app'>
       <div className="row d-flex align-items-center mt-4 mb-4">
-        <MovieListHeading heading='MyFlix'/>
+        <MovieListHeading heading='ShawnFlix'/>
         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
       </div>
       <div className="row">

06cf6acbd146528522cbc4181c5177e2fd501a49
Shawn Swineford <psswineford@gmail.com>
2022-09-21 14:39:37 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async (searchValue) => {
-     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=edbfcd53`
+     const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=edbfcd53`
     
      const response = await fetch(url);
      const responseJson = await response.json();
